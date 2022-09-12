a07c2351e9af39ed0a834624d46d7a989cd2666b
fcoverdugo <fcoverdugoa@underpost.net>
2022-09-12 14:49:35 -0400

    update

--- my-app/src/test/java/tarea4/FormTest.java
+++ my-app/src/test/java/tarea4/FormTest.java
@@ -23,4 +23,52 @@ public class FormTest extends TestCase {
         assertEquals(date, registroHoraPeluqueria.getDate());
     }
 
+    @DisplayName("Test Form.getType()")
+    @Test
+    public void testB() {
+        Context context = new Context();
+        String typeForm;
+        String date;
+        Form registroHoraPeluqueria;
+
+        typeForm = "Full";
+        date = "10/11/12 12:30";
+        registroHoraPeluqueria = new Form(
+                context, typeForm, date);
+
+        assertEquals(typeForm, registroHoraPeluqueria.getType());
+    }
+
+    @DisplayName("Test Form.getInfo()")
+    @Test
+    public void testC() {
+        Context context = new Context();
+        String typeForm;
+        String date;
+        Form registroHoraPeluqueria;
+
+        typeForm = "Full";
+        date = "06/05/12 09:30";
+        registroHoraPeluqueria = new Form(
+                context, typeForm, date);
+
+        assertEquals("CLP $8000, corte mas lavado y peinado.", registroHoraPeluqueria.getInfo());
+    }
+
+    @DisplayName("Test Form.getValidInfo()")
+    @Test
+    public void testD() {
+        Context context = new Context();
+        String typeForm;
+        String date;
+        Form registroHoraPeluqueria;
+
+        typeForm = "Simple";
+        date = "08/10/22 09:30";
+        registroHoraPeluqueria = new Form(
+                context, typeForm, date);
+
+        assertEquals(true, registroHoraPeluqueria.getValidInfo());
+    }
+
 }
